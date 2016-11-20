07686ab8c91a1864826c2631c284d5f509a4ff09
tuxito <alfredo.leon.fernandez@gmail.com>
2016-11-20 21:11:43 +0100

    request notification listener added

--- server.js
+++ server.js
@@ -1,6 +1,70 @@
 var express = require('express');
 var app = express();
 
+/* ------------------------------------------------------------------------------------------ */
+
+var firebase = require('firebase-admin');
+var request = require('request');
+
+var API_KEY = "AIzaSyDDlahLHMSVEifJm6rblDKHvHX-DueZgwg"; // Your Firebase Cloud Messaging Server API key
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("panarrap.json");
+
+// Initialize the app with a service account, granting admin privileges
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://testmaterial-d82a5.firebaseio.com/"
+});
+ref = firebase.database().ref();
+
+function listenForNotificationRequests() {
+  var requests = ref.child('notificationRequests');
+  ref.on('child_added', function(requestSnapshot) {
+    var request = requestSnapshot.val();
+    sendNotificationToUser(
+      request.username, 
+      request.message,
+      function() {
+        requestSnapshot.ref.remove();
+      }
+    );
+  }, function(error) {
+    console.error(error);
+  });
+};
+
+function sendNotificationToUser(username, message, onSuccess) {
+  request({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type' :' application/json',
+      'Authorization': 'key='+API_KEY
+    },
+    body: JSON.stringify({
+      notification: {
+        title: message
+      },
+      to : '/topics/user_'+username
+    })
+  }, function(error, response, body) {
+    if (error) { console.error(error); }
+    else if (response.statusCode >= 400) { 
+      console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+    }
+    else {
+      onSuccess();
+    }
+  });
+}
+
+// start listening
+listenForNotificationRequests();
+
+
+/* ------------------------------------------------------------------------------------------ */
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
@@ -21,3 +85,4 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
+
